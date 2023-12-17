6481dc6ba52e6c3435c259690f9064e49b28b7f0
anderson92zolis <azolis92@hotmail.es>
2023-12-17 13:53:48 +0100

    addidng the  Jason file to iterate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,23 @@ import React from "react"
 import Note from "./Note"
 import Footer from "./Footer"
 import Header from "./Header"
+import notes from "../notes"
+
+
 
 function App() {
-    return (
-      <div>
-        <Header/>
-        <Note/>
-        <Footer/>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (<Note
+        key={note.key}
+        title={note.title}
+        content={note.content}
+      />))}  {/*  here is not necesarry put again the Note because it is already in the Arrow function iterateNotes */}
+      <Footer />
+    </div>
+  )
+}
 
 
 export default App
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 
 
-function Note() {
-    return (
-      <div className="note">
-        <h1>First Note</h1>
-        <p>shop</p>
-      </div>
-    );
-  }
+const Note = (props) =>
+(
+  <div className="note">
+    <h1> {props.title}</h1>
+    <p> {props.content} </p>
+  </div>
+);
+
 
 export default Note
